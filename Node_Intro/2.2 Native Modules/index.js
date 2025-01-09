ca45fa62c399d70f973dab204b73df9275c8aa38
MacEwanStudent <sosar2@mymacewan.ca>
2025-01-09 11:38:43 -0700

    Learning npm

---
+++ Node_Intro/2.2 Native Modules/index.js
@@ -0,0 +1,16 @@
+//const fs =require("fs")
+
+/* fs.writeFile("message.txt", "Hello from NodeJS",  (err) => {
+    if (err) throw err;
+    console.log('The File has been saved');
+  }); */
+
+/* fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});  */
+
+var generateName = require("sillyname")
+var sillyName = generateName()
+
+console.log(sillyName)
