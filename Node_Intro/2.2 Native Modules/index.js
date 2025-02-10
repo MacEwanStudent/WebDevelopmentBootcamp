e837930a58f04071f1fd0e1ab8faa9c56012bbac
MacEwanStudent <sosar2@mymacewan.ca>
2025-02-10 12:47:50 -0700

    Intro to EJS, practice with npm packages and running express.

--- Node_Intro/2.2 Native Modules/index.js
+++ Node_Intro/2.2 Native Modules/index.js
@@ -9,8 +9,34 @@
   if (err) throw err;
   console.log(data);
 });  */
+import generateName from "sillyname";
+import {randomSuperhero} from 'superheroes';
+import qr from 'qr-image';
+import fs from 'fs'
 
-var generateName = require("sillyname")
-var sillyName = generateName()
+/* var sillyName = generateName()
+var nameSuper = randomSuperhero()
+console.log(`I am ${nameSuper}`) */
 
-console.log(sillyName)
+import inquirer from 'inquirer';
+
+var webSite= await inquirer
+  .prompt([{
+    type:'input',
+    name: 'URL',
+    message: 'Enter a Website:'
+  }])
+  .then((answers) => {
+    const urlVar = answers.URL;
+    var qr_svg = qr.image(urlVar)
+    qr_svg.pipe(fs.createWriteStream('qr_url.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
